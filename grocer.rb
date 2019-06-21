292b3e418c128ed3dca5fc07dc1e5e795ff09f51
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-21 14:09:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,14 @@
 def consolidate_cart(cart)
   # code here
-  
+    new_cart={}
+    cart.each do |item|
+      if new_cart.include?(item.keys.first)
+         new_cart[item.keys.first][:count] += 1
+      else
+         new_cart[item.keys.first] = item.values.first.merge({:count => 1})
+      end
+    end
+  new_cart
 end
 
 def apply_coupons(cart, coupons)
